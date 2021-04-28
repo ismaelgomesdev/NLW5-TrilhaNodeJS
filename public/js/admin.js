a3267b6613f251dbd052ac32c4ebb63f3572b2e6
Ismael <ismaelgomes@alu.ufc.br>
2021-04-28 19:42:58 -0300

    last day

--- public/js/admin.js
+++ public/js/admin.js
@@ -36,8 +36,51 @@ function call(id) {
   };
 
   socket.emit("admin_list_messages_by_user", params, (messages) => {
+    const divMessages = document.getElementById(
+      `allMessages${connection.user_id}`
+    );
 
-    const divMessages = document.getElementById()
+    messages.forEach((message) => {
+      const createDiv = document.createElement("div");
 
+      if (message.admin_id == null) {
+        createDiv.className = "admin_message_client";
+        createDiv.innerHTML = `<span>${connection.user.email}</span>`;
+        createDiv.innerHTML += `<span>${message.text}</span>`;
+        createDiv.innerHTML += `<span class="admin_date">${dayjs(
+          message.created_at
+        ).format("DD/MM/YYYY HH:mm:ss")}</span>`;
+      } else {
+        createDiv.className = "admin_message_admin";
+        createDiv.innerHTML = `Atendente:<span> ${message.text}</span>`;
+        createDiv.innerHTML += `<span class="admin_date">${dayjs(
+          message.created_at
+        ).format("DD/MM/YYYY HH:mm:ss")}</span>`;
+      }
+
+      divMessages.appendChild(createDiv);
+    });
   });
+}
+
+function sendMessage(id) {
+  const text = document.getElementById(`send_message_${id}`);
+
+  const params = {
+    text: text.value,
+    user_id: id,
+  };
+
+  socket.emit("admin_send_message", params);
+
+  const divMessages = document.getElementById(`allMessages${id}`);
+
+  const createDiv = document.createElement("div");
+  createDiv.className = "admin_message_admin";
+  createDiv.innerHTML = `Atendente:<span> ${params.text}</span>`;
+  createDiv.innerHTML += `<span class="admin_date">${dayjs().format("DD/MM/YYYY HH:mm:ss")}</span>`;
+
+  divMessages.appendChild(createDiv);
+
+  text.value = "";
 }
